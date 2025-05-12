466328526918f2738a0d2a7c7670381365339954
PixelPath7 <kevin.alarcon1@gmail.com>
2025-05-12 17:44:48 -0400

    Clase 12-05
    Se agrego promedio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const students = []
-const tableBody = document.querySelector("#studentsTable tbody")
+const tableBody = document.querySelector("#studentsTable tbody");
+const averageDiv=document.getElementById("average");
 
 document.getElementById("studentForm").addEventListener("submit", function(e){
     e.preventDefault();
@@ -7,21 +8,44 @@ document.getElementById("studentForm").addEventListener("submit", function(e){
     const name = document.getElementById("name").value.trim();
     const lastName = document.getElementById("lastName").value.trim();
     const grade = parseFloat(document.getElementById("grade").value);
+    const date = document.getElementById("date").value.trim();
 
-    if(!name || !lastName || isNaN(grade) || grade < 1 || grade > 7){
+    if(!name || !lastName || isNaN(grade) || grade < 1 || grade > 7 || !date){
         alert("Error al ingresar Datos")
         return
     }
 
-    const student = {name, lastName, grade, fecha}
+    const student = {name, lastName, grade, date}
 
     students.push(student);
     //console.log(students)
     addStudentToTable(student)
+    calculateAverage()
 
     this.reset()
 
 });
+
+function calculateAverage(){
+    let total_notas = 0
+    let total_estudiantes = students.length
+
+    for(i of students){
+        console.log(i);
+        total_notas = i.grade + total_notas
+    }
+    averageDiv.innerHTML = `Promedio General del Curso: ${total_notas/total_estudiantes}`
+}
+
+//function calcularPromedio(){
+//    if(students.length===0){
+//        averageDiv.textContent = "Promedio General del Curso: N/A"
+//        return
+//    }
+//    const total=students.reduce((sum, student) => sum + student.grade, 0);
+//    const prom = total/students.length;
+//    averageDiv.textContent = "Promedio General del Curso: "+prom.toFixed(2);
+//}
 
 function addStudentToTable(student){
     const row = document.createElement("tr");
@@ -29,6 +53,6 @@ function addStudentToTable(student){
         <td> ${student.name}</td>
         <td> ${student.lastName}</td>
         <td> ${student.grade}</td>
-        <td> ${student.fecha}</td>`;
+        <td> ${student.date}</td>`;
     tableBody.appendChild(row);
 }
